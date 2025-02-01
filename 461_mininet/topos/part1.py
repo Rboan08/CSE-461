90a1edc3c6ccd6386691a9dfa2d0d65001bb9877
gersaldo <gersaldo@uw.edu>
2025-02-01 13:31:18 -0800

    part 1 complete

--- 461_mininet/topos/part1.py
+++ 461_mininet/topos/part1.py
@@ -8,10 +8,20 @@ from mininet.cli import CLI
 
 class part1_topo(Topo):
     def build(self):
-        pass
         # switch1 = self.addSwitch('switchname')
         # host1 = self.addHost('hostname')
         # self.addLink(hostname,switchname)
+        host1 = self.addHost('h1')
+        host2 = self.addHost('h2')
+        host3 = self.addHost('h3')
+        host4 = self.addHost('h4')
+
+        switch1 = self.addSwitch('s1')
+	
+        self.addLink('h1', 's1')
+        self.addLink('h2', 's1')
+        self.addLink('h3', 's1')
+        self.addLink('h4', 's1')
 
 
 topos = {"part1": part1_topo}
